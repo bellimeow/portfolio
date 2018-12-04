26b80abcdfbaa2ae56dbedd67a7b97186e57f772
Isabella Delgado <isade842@student.liu.se>
2018-12-04 22:42:13 +0100

    changed author information and added projects

--- handledata.py
+++ handledata.py
@@ -3,7 +3,8 @@ import json
 
 
 def load(filename):
-    '''Loads a file and returns a list of all the projects sorted by project id:s.'''
+    '''Loads a file and returns a list of all the projects sorted by project
+    id:s.'''
     try:
         with open(filename, encoding="utf-8") as data_file:
             data = json.load(data_file)
@@ -13,7 +14,7 @@ def load(filename):
             return sorted_data
     except:
         return None
-    
+
 
 
 
@@ -49,12 +50,15 @@ def get_techniques(db):
 
 
 def get_technique_stats(db):
-    '''Returns a dictionary containing techniques and the projects in which they are used. The projects are represented only by their id and name.'''
+    '''Returns a dictionary containing techniques and the projects in which they
+     are used. The projects are represented only by their id and name.'''
     technique_stats = {}
 
     for project in db:
-        # For each project we insert information about the project's id and name.
-        project_info = {'id': project['project_id'], 'name': project['project_name']}
+        # For each project we insert information about the project's id and
+        # name.
+        project_info = {'id': project['project_id'],
+                        'name': project['project_name']}
 
         techniques_used = project.get('techniques_used', None)
         if techniques_used:
@@ -68,8 +72,12 @@ def get_technique_stats(db):
 
 
 
-def search(db, sort_by = 'start_date', sort_order = 'desc', techniques = None, search = None, search_fields = None):
-    '''Returns a list of projects that have a field that contains the specified user input and sort by any field, either ascending or descending. Defaults to sorting by start date. If search_fields is None, then all the projects fields are searched and returned.'''
+def search(db, sort_by = 'start_date', sort_order = 'desc', techniques = None,
+            search = None, search_fields = None):
+    '''Returns a list of projects that have a field that contains the specified
+    user input and sort by any field, either ascending or descending. Defaults
+    to sorting by start date. If search_fields is None, then all the projects
+    fields are searched and returned.'''
     matching_projects = []
 
     # If search is None then return all projects.
@@ -77,7 +85,7 @@ def search(db, sort_by = 'start_date', sort_order = 'desc', techniques = None, s
         search = ""
 
     search = search.lower()
-    
+
     # First find all matching projects.
     for project in db:
         if search_in_project(project, techniques, search, search_fields):
@@ -104,15 +112,18 @@ def search_in_project(project, techniques, search, search_fields):
                 return False
 
     if isinstance(search_fields, list) and len(search_fields) == 0:
-        # If list of search fields are empty (but not None!), no search can be done
+        # If list of search fields are empty (but not None!), no search can be
+        # done
         return False
     elif not search_fields:
-        return recursive_search_object(project, search) 
+        return recursive_search_object(project, search)
     else:
-        # Check for search in each of the search fields. If any of fields contain the search string, return True.
+        # Check for search in each of the search fields. If any of fields
+        # contain the search string, return True.
         for search_field in search_fields:
             project_field = project.get(search_field, None)
-            if search_field != None and recursive_search_object(project_field, search):
+            if search_field != None and
+                recursive_search_object(project_field, search):
                 return True
 
     return False
@@ -120,7 +131,8 @@ def search_in_project(project, techniques, search, search_fields):
 
 
 def recursive_search_object(obj, search):
-    '''Recursively searches any object, if object is a container then it's children will be searched instead.'''
+    '''Recursively searches any object, if object is a container then it's
+    children will be searched instead.'''
 
     if isinstance(obj, (tuple, list, dict)):
         # If object is a dict, ignore it's keys.
@@ -133,19 +145,25 @@ def recursive_search_object(obj, search):
 
         return False
 
-    # If the object happens to be an integer or other non-string type, then convert it to a string and search it.
+    # If the object happens to be an integer or other non-string type, then
+    # convert it to a string and search it.
     return search in str(obj).lower()
 
 
 
 def sort_projects(db, sort_by, sort_order):
-    '''Returns a list of all the projects sorted by a specified field and in a specified order.'''
+    '''Returns a list of all the projects sorted by a specified field and in a
+    specified order.'''
 
     sort_descending = sort_order == 'desc'
 
-    # '\uffff' is default key when sorted by ascending. \uffff represents the highest possible unicode character.
-    # '\u0000' is default key when sorted by descending. \u0000 represents the lowest possible unicode character.
+    # '\uffff' is default key when sorted by ascending.
+    # \uffff represents the highest possible unicode character.
+    # '\u0000' is default key when sorted by descending.
+    # \u0000 represents the lowest possible unicode character.
     missing_key = '\u0000' if sort_descending else '\uffff'
 
-    # If the search field is not present in the project then always sort it last.
-    return sorted(db, key = lambda k: k[sort_by] if sort_by in k else missing_key, reverse = sort_descending)
+    # If the search field is not present in the project then always sort it
+    # last.
+    return sorted(db, key = lambda k: k[sort_by] if sort_by in k else
+                  missing_key, reverse = sort_descending)
--- myFlaskProject.py
+++ myFlaskProject.py
@@ -1,8 +1,4 @@
-from flask import Flask
-from flask import render_template
-from flask import url_for
-from flask import request
-from flask import abort
+from flask import Flask, render_template, url_for, request, abort
 import os
 import handledata
 import re
@@ -17,11 +13,19 @@ PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
 
 @app.route("/")
 def website_index():
-    '''A route to the website index page named "/" with Flask. The index website displays a maximum of five of the newest projects and the latest projects based on project id are collected from the database in a new list. The database is sorted in a reversed order, since the newest project will have the highest project id. The information gathered are the modified database as well as the information about the author of the portfolio. This returns a Jinja2 rendered template with the gathered information with a Flask built in function.'''
+    '''A route to the website index page named "/" with Flask. The index website
+     displays a maximum of five of the newest projects and the latest projects
+     based on project id are collected from the database in a new list. The
+     database is sorted in a reversed order, since the newest project will have
+     the highest project id. The information gathered are the modified database
+     as well as the information about the author of the portfolio. This returns
+     a Jinja2 rendered template with the gathered information with a Flask built
+      in function.'''
     db = get_data()
     author = get_author_data()
 
-    #Check to see if the database with projects is longer than five projects. The index website displays the five newest projects sorted by project ID.
+    # Check to see if the database with projects is longer than five projects.
+    # The index website displays the five newest projects sorted by project ID.
     if len(db)> 5:
         db = db[-5:]
     db.reverse()
@@ -33,7 +37,11 @@ def website_index():
 
 @app.route("/list")
 def project_list():
-    '''A Route for the project list page named "/list page" with Flask. Saves information about the current search possibilities, current search bar string and other search parameters selected by the user. This returns a Jinja2 rendered template with the gathered information with a Flask built in function.'''
+    '''A Route for the project list page named "/list page" with Flask. Saves
+    information about the current search possibilities, current search bar
+    string and other search parameters selected by the user. This returns a
+    Jinja2 rendered template with the gathered information with a Flask built in
+     function.'''
 
     # Get all search possibilities for the project list.
     technique_list = get_all_techniques()
@@ -46,36 +54,51 @@ def project_list():
     # Get selected techniques, search fields, sort by field and sort by order.
     selected_techniques = get_selected_techniques_for_search()
     selected_search_fields = get_selected_fields_for_search()
-    selected_search_fields = None if len(selected_search_fields) == 0 else selected_search_fields
+    selected_search_fields = None if len(selected_search_fields) == 0
+                                  else selected_search_fields
     selected_sortby_field = get_selected_sortby_field_for_search()
     selected_search_order_field = get_selected_sortorder_field_for_search()
 
 
     db = get_data()
 
-    db = handledata.search(db, sort_by=selected_sortby_field, sort_order=selected_search_order_field, techniques=selected_techniques, search=search_string, search_fields=selected_search_fields)
+    db = handledata.search(db, sort_by=selected_sortby_field,
+         sort_order=selected_search_order_field, techniques=selected_techniques,
+         search=search_string, search_fields=selected_search_fields)
 
     db = set_default_values(db)
 
 
-    return render_template("projectlist.html", db = db, technique_list = technique_list, search_fields=search_field_list, sortby_fields = sortby_field_list)
+    return render_template("projectlist.html", db = db,
+                           technique_list = technique_list,
+                           search_fields=search_field_list,
+                           sortby_fields = sortby_field_list)
 
 
 @app.route("/techniques")
 def project_techniques():
-    '''A Route for the technique page named "/techniques" with Flask. Information about the selected techniques on the website are gathered in a list, the information about the projects in the database that used the selected techniques are gathered in a list. There is also a list containing all the technique used in all projects, this is mainly used to display all the technique buttons on the website. This returns a Jinja2 rendered template with the gathered information with a Flask built in function.'''
+    '''A Route for the technique page named "/techniques" with Flask.
+    Information about the selected techniques on the website are gathered in a
+    list, the information about the projects in the database that used the
+    selected techniques are gathered in a list. There is also a list containing
+    all the technique used in all projects, this is mainly used to display all
+    the technique buttons on the website. This returns a Jinja2 rendered
+    template with the gathered information with a Flask built in function.'''
 
-    # Get the selected values from the technique page and split each search value into a list.
+    # Get the selected values from the technique page and split each search
+    # value into a list.
     args = get_request_args('selected_techniques')
     selected_techniques = args['selected_techniques']
-    selected_techniques_list = selected_techniques.split(',') if selected_techniques else []
+    selected_techniques_list = selected_techniques.split(',')
+                               if selected_techniques else []
 
     # Get the project from the database that used the selected techniques.
     project_list = get_projects_by_selected_techniques(selected_techniques_list)
     project_list = set_default_values(project_list)
 
     # Gather all used techniques from all the project in the database.
-    technique_list = [{'label': technique, 'selected': technique in selected_techniques_list}
+    technique_list = [{'label': technique, 'selected': technique in
+                        selected_techniques_list}
                       for technique in get_all_techniques()]
 
     # Design on technique buttons.
@@ -84,17 +107,23 @@ def project_techniques():
     # Generate form value for each button
     for technique in technique_list:
         if selected_techniques and technique['selected'] == True:
-            technique['form_value'] = form_value_remove_item(selected_techniques, technique['label'])
+            technique['form_value'] =
+                form_value_remove_item(selected_techniques, technique['label'])
         else:
-            technique['form_value'] = form_value_add_item(selected_techniques, technique['label'])
+            technique['form_value'] =
+                form_value_add_item(selected_techniques, technique['label'])
 
-    return render_template("techniques.html", technique_list = technique_list, project_list = project_list)
+    return render_template("techniques.html", technique_list = technique_list,
+                            project_list = project_list)
 
 
 
 @app.route("/project/<int:id>")
 def show_project(id = None):
-    '''A route for a specific project page named "/project/<int:id>" with Flask. This collects all the data for a specific project and collects the name of the project images from the mapstructure.  This returns a Jinja2 rendered template with the gathered information with a Flask built in function.'''
+    '''A route for a specific project page named "/project/<int:id>" with Flask.
+     This collects all the data for a specific project and collects the name of
+     the project images from the mapstructure.  This returns a Jinja2 rendered
+     template with the gathered information with a Flask built in function.'''
 
     project = [get_project(id)]
 
@@ -104,10 +133,7 @@ def show_project(id = None):
     project_list = set_default_values(project)
     project = project_list[0]
 
-    # Checking if image file has a valid address. Might want to get a check to see that the file exists instead
-
     return render_template("projectpage.html", project=project)
-
 
 
 
@@ -126,18 +152,29 @@ def get_project(id):
 
 
 def get_all_techniques():
-    '''Returns all techniques as a list of strings from the default json database.'''
+    '''Returns all techniques as a list of strings from the default json
+    database.'''
     db = get_data()
     return handledata.get_techniques(db)
 
 def get_all_search_fields():
     '''Returns all search fields that can be selected on the search page.'''
-    search_fields = [("project_name", "Project name"), ("start_date", "Start date"), ("end_date", "End date"), ("group_size", "Group size"), ("short_description", "Short description"), ("long_description", "Long description"), ("academic_credits", "Academic credits"), ("course_id", "Course ID"), ("course_name", "Course name")]
+    search_fields = [("project_name", "Project name"),
+                    ("start_date", "Start date"), ("end_date", "End date"),
+                    ("group_size", "Group size"),
+                    ("short_description", "Short description"),
+                    ("long_description", "Long description"),
+                    ("academic_credits", "Academic credits"),
+                    ("course_id", "Course ID"), ("course_name", "Course name")]
     return search_fields
 
 def get_all_sortby_fields():
     '''Returns all fields that can be sorted by on projectlist page'''
-    sortby_fields = [("start_date", "Start date"), ("project_name", "Project name"), ("end_date", "End date"), ("group_size", "Group size"), ("academic_credits", "Academic credits"), ("course_id", "Course ID"), ("course_name", "Course name")]
+    sortby_fields = [("start_date", "Start date"),
+                    ("project_name", "Project name"),
+                    ("end_date", "End date"), ("group_size", "Group size"),
+                    ("academic_credits", "Academic credits"),
+                    ("course_id", "Course ID"), ("course_name", "Course name")]
     return sortby_fields
 
 
@@ -172,7 +209,8 @@ def get_selected_sortby_field_for_search():
     return arg["sortby"]
 
 def get_selected_sortorder_field_for_search():
-    '''Returns the selected order for sorting the results on the projectlist page. If no order has been chosen, the default is descending.'''
+    '''Returns the selected order for sorting the results on the projectlist
+    page. If no order has been chosen, the default is descending.'''
     arg = get_request_args("sortorder")["sortorder"]
 
     if arg==None:
@@ -182,7 +220,9 @@ def get_selected_sortorder_field_for_search():
 
 
 def get_projects_by_selected_techniques(selected_techniques):
-    ''' Returns all projects from the default json database that used all techiques in the passed list of techniques. If the passed list is None all projects will be returned.'''
+    ''' Returns all projects from the default json database that used all
+    techiques in the passed list of techniques. If the passed list is None all
+    projects will be returned.'''
     db = get_data()
     return handledata.search(db, techniques = selected_techniques)
 
@@ -193,18 +233,24 @@ def get_author_data():
     return json.loads(open(json_url, encoding="utf-8").read())
 
 def set_default_values(db):
-    '''Looks for a valid image type in big_image and small_image in a database, and a valid image type is as long as the type name contains 3 to 4 letters. If there aren't a valid picture for a project a default image is set for the project. Otherwise a picture named after the project ID number is set. This only modifies a copy of the database and returns it. '''
+    '''Looks for a valid image type in big_image and small_image in a database,
+    and a valid image type is as long as the type name contains 3 to 4 letters.
+    If there aren't a valid picture for a project a default image is set for the
+     project. Otherwise a picture named after the project ID number is set. This
+      only modifies a copy of the database and returns it. '''
     db_copy = copy.deepcopy(db)
     for project in db_copy:
         if not re.search("\w+\.\w{3,4}", project["big_image"]):
             project["big_image"] = "Programming-languages.jpg"
         else:
-            project["big_image"] = "project" + str(project["project_id"]) + "/" + project["big_image"]
+            project["big_image"] = "project" + str(project["project_id"]) +
+                                   "/" + project["big_image"]
 
         if not re.search("\w+\.\w{3,4}", project["small_image"]):
             project["small_image"] = "No_image_available.svg"
         else:
-            project["small_image"] = "project" + str(project["project_id"]) + "/" + project["small_image"]
+            project["small_image"] = "project" + str(project["project_id"]) +
+                                     "/" + project["small_image"]
 
     return db_copy
 
@@ -213,7 +259,10 @@ def set_default_values(db):
 # --- HTTP Related Functions ---
 
 def get_request_args(*arg_names):
-    '''Specific parameters from the GET or POST request can be extracted. The wanted parameter(s) are passed into the function, and the parameters with their values are returned in a dictionary.  Accepts any number of string values - one for each key.'''
+    '''Specific parameters from the GET or POST request can be extracted. The
+    wanted parameter(s) are passed into the function, and the parameters with
+    their values are returned in a dictionary.  Accepts any number of string
+    values - one for each key.'''
     arg_table = {}
     if request.method == 'GET':
         for arg in arg_names:
@@ -227,7 +276,8 @@ def get_request_args(*arg_names):
 
 
 def form_value_add_item(form_value, item):
-    '''Appends a string to a comma separated GET value string. X,Y -> X,Y,<item>.'''
+    '''Appends a string to a comma separated GET value string.
+    X,Y -> X,Y,<item>.'''
     if item == None or len(item) == 0:
         return
 
@@ -243,7 +293,8 @@ def form_value_add_item(form_value, item):
     return ','.join(item_list)
 
 def form_value_remove_item(form_value, item):
-    '''Removes a string from a comma separated GET value string. X,<item>,Y -> X,Y.'''
+    '''Removes a string from a comma separated GET value string.
+    X,<item>,Y -> X,Y.'''
     if form_value == None or len(form_value) == 0:
         return ""
 
@@ -254,10 +305,13 @@ def form_value_remove_item(form_value, item):
 
 # --- Style Functions --
 
-techniques_decorative_wing_pairs = [('<', '>'), ('(', ')'), ('[', ']'), ('{', '}'), ('/', '/'), ('.', '.'), ('%', '%')]
+techniques_decorative_wing_pairs = [('<', '>'), ('(', ')'), ('[', ']'),
+                                    ('{', '}'), ('/', '/'), ('.', '.'),
+                                    ('%', '%')]
 
 def assign_random_wing_pair_to_technique_list(technique_list):
-    '''Iterates a list of technique dictionaries and adds a set of matching characters to each dictionary with the keys 'left_wing' and 'right_wing'.'''
+    '''Iterates a list of technique dictionaries and adds a set of matching
+    characters to each dictionary with the keys 'left_wing' and 'right_wing'.'''
     if technique_list == None or len(technique_list) == 0:
         return
 
@@ -269,7 +323,8 @@ def assign_random_wing_pair_to_technique_list(technique_list):
     # Transform each element in technique_list.
     for i in range(len(technique_list)):
         # Assign each element a random pair of "wings".
-        wing_pair = techniques_decorative_wing_pairs[index_to_decorative_wings[i] % len(techniques_decorative_wing_pairs)]
+        wing_pair = techniques_decorative_wing_pairs[
+        index_to_decorative_wings[i] % len(techniques_decorative_wing_pairs)]
 
         #decoration_map[technique_list[i]] = wing_pair
         technique_list[i]['left_wing'] = wing_pair[0]
@@ -279,9 +334,9 @@ def assign_random_wing_pair_to_technique_list(technique_list):
 # --- Super Generic Help Functions ---
 import random
 
-
 def randomize_list(lst):
-    '''Iterates a list and swaps each element with another element at a random position in the list. The passed list is transformed!'''
+    '''Iterates a list and swaps each element with another element at a random
+    position in the list. The passed list is transformed!'''
     if lst == None or len(lst) <= 1:
         return
 
